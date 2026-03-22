5468045da0378c7cda2513d8f9d9f9305e1ec55d
DeeJai Software <sompornjanthra@gmail.com>
2026-03-22 18:18:03 +0700

    Initial Upload

---
+++ l10n_th_thai_provinces/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Part of DeeJai Software. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Thailand Province Thai Translation',
+    'version': '18.0.1.0.0',
+    'category': 'Localization',
+    'summary': 'Update Thailand province names from English to Thai',
+    'description': """
+Thailand Province Thai Translation
+====================================
+This module updates the default English names of Thailand's 77 provinces to their official Thai names.
+    """,
+    'author': 'DeeJai Software',
+    'website': 'https://www.deejaisoftware.com',
+    'license': 'OPL-1',
+    'depends': ['base'],
+    'data': [
+        'data/res_country_state_data.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
